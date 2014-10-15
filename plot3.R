a098aa0aa8c983f04a3540e607e2e97cc737bcfb
Octavio Echeverri <octavioecheverri@gmail.com>
2014-10-14 22:19:13 -0500

    Plot 3 code and Plot 2 png

--- plot3.R
+++ plot3.R
@@ -1,3 +1,4 @@
+## Getting full dataset
 data_full <- read.csv("./Data/household_power_consumption.txt", header=T, sep=';', na.strings="?", 
                       nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
 data_full$Date <- as.Date(data_full$Date, format="%d/%m/%Y")
@@ -10,14 +11,16 @@ rm(data_full)
 datetime <- paste(as.Date(data$Date), data$Time)
 data$Datetime <- as.POSIXct(datetime)
 
-## Plot 2
-plot(data$Global_active_power~data$Datetime, type="l",
-     ylab="Global Active Power (kilowatts)", xlab="")
-dev.copy(png, file="plot2.png", height=480, width=480)
-dev.off()
+## Plot 3
+with(data, {
+        plot(Sub_metering_1~Datetime, type="l",
+             ylab="Global Active Power (kilowatts)", xlab="")
+        lines(Sub_metering_2~Datetime,col='Red')
+        lines(Sub_metering_3~Datetime,col='Blue')
+})
+legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, 
+       legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
 ## Saving to file
-dev.copy(png, file="plot1.png", height=480, width=480)
+dev.copy(png, file="plot3.png", height=480, width=480)
 dev.off()
-
-## File closed
